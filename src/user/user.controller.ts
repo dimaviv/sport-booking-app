beaa06df5ca85b392fc2fc16b97600d42c623149
dimaviv <dimaviv1812@gmail.com>
2024-05-10 23:06:21 +0300

    test: deeplink verify-email

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Query, Res, HttpStatus } from '@nestjs/common';
-import { Response } from 'express';
+import {Controller, Get, Query, Res, HttpStatus, Req, Next} from '@nestjs/common';
+import { Response, Request } from 'express';
 import { UserService } from './user.service';
+const deeplink = require('node-deeplink');
 
 @Controller('user')
 export class UserController {
@@ -9,18 +10,47 @@ export class UserController {
     @Get('verify-email')
     async verifyEmail(
         @Query('token') token: string,
+        @Next() next,
+        @Req() req: Request,
         @Res() res: Response
     ) {
         try {
-            const result = await this.userService.verifyEmailToken(token);
+            //const result = await this.userService.verifyEmailToken(token);
+            const result = true;
+            const userAgent = req.headers['user-agent'];
+            console.log(userAgent)
 
             if (result) {
-                return res.redirect(`${process.env.APP_URL}/email-verification-success`);
+                if (this.isMobileDevice(userAgent)) {
+
+                    console.log(`${process.env.APP_URL}/mobile-app-download`)
+                    console.log(process.env.ANDROID_PACKAGE_NAME)
+                    console.log(process.env.IOS_STORE_LINK)
+
+                    return deeplink({
+                        fallback: `${process.env.APP_URL}/mobile-app-download`,
+                        android_package_name: `${process.env.ANDROID_PACKAGE_NAME}`,
+                        ios_store_link: `${process.env.IOS_STORE_LINK}`,
+                        // delay: 1000,
+                        // callback: null,
+                    })(req, res, next);
+
+                } else {
+                    return res.redirect(`${process.env.APP_URL}/email-verification-success`);
+                }
             } else {
+               // return res.redirect(`${process.env.APP_URL}/email-verification-error`);
                 res.status(HttpStatus.BAD_REQUEST).json({ message: 'Invalid or expired token.' });
             }
         } catch (error) {
+            console.log(error)
+            // return res.redirect(`${process.env.APP_URL}/email-verification-error`);
             res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Server error.', error });
         }
+    }
+
+    private isMobileDevice(userAgent: string): boolean {
+        const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+        return mobileRegex.test(userAgent);
     }
 }
