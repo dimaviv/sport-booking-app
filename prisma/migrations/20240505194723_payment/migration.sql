4c4889f07573c47985c85d470bb03474a353896b
dimaviv <dimaviv1812@gmail.com>
2024-05-10 16:01:07 +0300

    fix: change payment migration

--- prisma/migrations/20240505194723_payment/migration.sql
+++ prisma/migrations/20240505194723_payment/migration.sql
@@ -43,5 +43,3 @@ CREATE TABLE "Payment" (
 -- AddForeignKey
 ALTER TABLE "Payment" ADD CONSTRAINT "Payment_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "Booking"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
--- AddForeignKey
-ALTER TABLE "Payment" ADD CONSTRAINT "Payment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
