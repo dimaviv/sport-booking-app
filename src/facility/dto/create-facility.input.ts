03c4c1b6de0d6e260b902d69556b50d4dd7cc465
dimaviv <dimaviv1812@gmail.com>
2024-05-13 20:58:20 +0300

    fix: fix validation for creating facility

--- src/facility/dto/create-facility.input.ts
+++ src/facility/dto/create-facility.input.ts
@@ -1,47 +1,48 @@
-import {InputType, Int, Field, registerEnumType} from '@nestjs/graphql';
+import {InputType, Field, registerEnumType} from '@nestjs/graphql';
 import {covering_type, facility_type, sport_type} from "@prisma/client";
-import {IsDate, IsInt, IsOptional, IsString} from "class-validator";
+import {IsArray, IsEnum, IsInt, IsString} from "class-validator";
 
 @InputType()
 export class CreateFacilityInput {
 
-  @IsString({message: "Must be string"})
+  @IsString({message: "Name must be a string"})
   @Field()
   readonly name: string;
 
-  @IsString({message: "Must be string"})
+  @IsInt({ message: 'District ID must be an integer' })
   @Field()
   readonly districtId: number;
 
-  @IsString({message: "Must be string"})
+  @IsString({message: "Address must be a string"})
   @Field()
   readonly address: string;
 
-  // @IsString({message: "Must be string"})
+  @IsArray({ message: 'Sport types must be an array' })
+  @IsEnum(sport_type, { each: true, message: 'Each sport type must be a valid enum value' })
   @Field(() => [sport_type])
   readonly sportType: sport_type[];
 
-  @IsString({message: "Must be string"})
+  @IsString({message: "Covering type must be a string"})
   @Field(() => covering_type)
   readonly coveringType: covering_type;
 
-  @IsString({message: "Must be string"})
+  @IsString({message: "Facility type must be a string"})
   @Field(() => facility_type)
   readonly facilityType: facility_type;
 
-  @IsString({message: "Must be string"})
+  @IsString({message: "Description must be a string"})
   @Field({ nullable: true })
   readonly description?: string;
 
-  @IsString({message: "Must be string"})
+  @IsString({message: "Location must be a string"})
   @Field({ nullable: true })
   readonly location?: string;
 
-  @IsInt({ message: 'Must be int' })
+  @IsInt({ message: 'Minimum booking time must be an integer'})
   @Field({ nullable: true })
   readonly minBookingTime?: number;
-
 }
+
 
 
 registerEnumType(sport_type, {
