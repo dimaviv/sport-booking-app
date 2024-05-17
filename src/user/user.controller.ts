38208fe8219d48c338fb3bcf6cfa94ee1f1bb650
dimaviv <dimaviv1812@gmail.com>
2024-05-17 18:28:03 +0300

    fix: change deeplink url

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -29,13 +29,15 @@ export class UserController {
                     console.log(process.env.IOS_STORE_LINK)
 
                     const dp = deeplink({
-                        fallback: `${process.env.APP_URL}/link/email-confirmation`,
+                        fallback: `${process.env.APP_URL}/mobile-app-download`,
                         android_package_name: `${process.env.ANDROID_PACKAGE_NAME}`,
                         ios_store_link: `${process.env.IOS_STORE_LINK}`,
+                        url: `${process.env.APP_URL}/link/email-confirmation`, // Specify the deeplink URL here
                         // delay: 1000,
                         // callback: null,
                     })(req, res, next);
-                    return dp
+
+                    return dp;
                 } else {
                     return res.redirect(`${process.env.APP_URL}/email-verification-success`);
                 }
